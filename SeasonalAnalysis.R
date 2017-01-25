93d5eb6e7d00ed7eed03eb5480e4c8f7ebcfb6ce
unknown <cem@HACIVAT.redmond.corp.microsoft.com>
2017-01-24 19:46:38 -0800

    Code update

--- SeasonalAnalysis.R
+++ SeasonalAnalysis.R
@@ -1,69 +1,119 @@
-#Get the library 
-library("dplyr")
-library("XML")
-library("RMySQL")
+library(dplyr)
+library(lubridate)
+library(Quandl)
+library(RMySQL)
 
-#Read csv file
-#blob <- read.csv("c:\\temp\\CHRIS-CME_NG11.csv", header=TRUE)  
+myhost <- "cemoptions.cloudapp.net"
 
-#Set the ticker
-stockticker <- "NG"
-
-#Connection settings and the dataset
-mydb <- dbConnect(MySQL(), user='borsacanavari', password='opsiyoncanavari1', dbname='myoptions', host='cemoptions.cloudapp.net')
-rs <- dbSendQuery(mydb, paste("SELECT F.SNAPSHOTDATE AS 'Date', F.OPEN AS 'Open', F.High, F.Low, F.LAST AS 'Last', F.CHANGE AS 'Change', F.Settle FROM futures F WHERE F.Future = '",stockticker,"' ORDER BY F.SNAPSHOTDATE DESC",sep=""))
-blob <- fetch(rs, n=-1)
-
-#Close the database
-dbDisconnect((mydb))
-
-#Set the dates
-blob$Date <- as.POSIXct(blob$Date)
-
-#Group them based on their dates, 1st of month
-pdates <- blob[,c(1,2)] %>% 
-  group_by(month(as.POSIXlt(blob$Date)),year(as.POSIXlt(blob$Date))) %>% 
-  slice(which.min(as.numeric(as.POSIXlt(Date))))
-
-#Get the dates with the first
-pdates <- as.list(pdates[with(pdates, order(as.character(as.POSIXlt(Date)))), 1])
-pdates <- as.vector(as.character(as.POSIXlt(pdates$Date)))
-
-#These are the line numbers
-nline <- as.numeric(rownames(blob[as.character(blob$Date) %in% pdates, ]))
-
-#Create a temp data frame
-mdata <- data.frame(
-    SnapshotDate = character(),
-    Open = numeric(),
-    PSettle = numeric()
-    )
-
-#Loop on lines
-for(i in 1:length(nline))
+#Check if the file exists, it not use a constant. 
+if(file.exists("/home/cem/portakal/futures.csv"))
 {
-    mdata <- rbind(mdata, 
-                data.frame(
-                  SnapshotDate = blob[nline[i],1],
-                  Open = blob[nline[i],2],
-                  PSettle = if(i==1) blob[nline[i],2] else blob[nline[i-1],7] 
-                )
-  )
+  futuresblob <- read.csv("/home/cem/portakal/futures.csv", header=FALSE)  # read csv file 
+  stocktickervector <- sort(as.vector(futuresblob[,1]))
+} else
+{
+  #stocktickervector <- sort(c("ZB","NG","ES","6J","6A","6B","CL","SB","6E","GC","SI"))
+  #stocktickervector <- sort(c("GC","NG"))
+  futuresblob <- read.csv("c:\\temp\\futures.csv", header=FALSE, stringsAsFactors=FALSE)  # read csv file 
+  stocktickervector <- futuresblob[order(futuresblob$V1),c(1,2)]
 }
 
-#Temp vector
-v <- c()
-
-#Calculate difference 
-for(i in 1:nrow(mdata)-1)
+#Write to the database
+for(i in 1:nrow(stocktickervector)) 
 {
-  v <- c(v,(mdata[i,3]-mdata[i+1,2])/mdata[i+1,2]) #Month open vs month settle
+  #Connection settings and the dataset
+  mydb = dbConnect(MySQL(), user='borsacanavari', password='opsiyoncanavari1', dbname='myoptions', host=myhost)
+  
+  #Get the ticker
+  stockticker <- stocktickervector[i,1] #Get the symbol
+  
+  #Get the futures
+  myfuture <- Quandl(paste("CHRIS/",stocktickervector[i,2],sep=""), api_key="zK6coAV1K5eyxuaPvWJm")
+
+  #Loop on the values pulled  
+  for(x in 1:nrow(myfuture)) 
+  {
+    if(!is.null(myfuture$Last[x]) && !is.na(myfuture$Last[x]) && myfuture$Last[x] != "NA")
+    {
+      query <- paste("INSERT INTO futuresdata VALUES('",stockticker,"','",myfuture$Date[x],"',",myfuture$Last[x],")",sep="")
+      print(query)
+      dbSendQuery(mydb,query)
+    }
+  }
+  
+  #Close the database
+  dbDisconnect((mydb))
 }
 
-#Add the differences 
-mdata$PDifference <- c(NA,v)
+#Loop on the futures
+for(i in 1:nrow(stocktickervector))
+{
+  #Get the ticket
+  stockticker <- stocktickervector[i,1] #Get the symbol
+  print(stockticker) #For debugging
+  
+  #Get the futures
+  #myfuture <- Quandl(paste("CHRIS/",stocktickervector[i,2],sep=""), api_key="zK6coAV1K5eyxuaPvWJm")
+  #Connection settings and the dataset
+  mydb = dbConnect(MySQL(), user='borsacanavari', password='opsiyoncanavari1', dbname='myoptions', host=myhost)
+  rs = dbSendQuery(mydb, paste("SELECT P.* FROM futuresdata P WHERE P.FUTURE = '",stockticker,"'",sep=""))
+  myfuture = fetch(rs, n=-1)
+  
+  #Find the end of months
+  mymonth <- myfuture %>% 
+    group_by(strftime(SNAPSHOTDATE, "%Y-%m")) %>% #Groups by the yearmonths
+    filter(SNAPSHOTDATE == max(SNAPSHOTDATE)) %>% #Take the last date of each group
+    .$SNAPSHOTDATE  
+  
+  #Filter the dataset to just end of month data. 
+  myfuture <- myfuture[myfuture$SNAPSHOTDATE %in% mymonth,]
+  myfuture$SNAPSHOTDATE <- as.Date(myfuture$SNAPSHOTDATE)
+  
+  #Filter the data to jan start
+  myfuture <- myfuture[myfuture$SNAPSHOTDATE > as.Date("2000-12-31"),]
+  myfuture <- myfuture[2:nrow(myfuture),] #Leave the current month out
+  
+  #Dummy vector
+  b <- as.vector(0)
+  
+  #Loop on the rows
+  for(i in 1:nrow(myfuture)-1)
+  { b[i] <- myfuture$LAST[i+1] }
+  
+  #Add the last values as a column
+  myfuture$PREVLAST <- c(b,NA)
+  
+  #Remove the last row with na
+  myfuture <- myfuture[1:nrow(myfuture)-1,] 
+  
+  #Remove the last row with na
+  myfuture$DIFF <- (myfuture[,3] - myfuture[,4])/myfuture[,4]*100
+  
+  #Add the months
+  myfuture$MONTH <- month(myfuture$SNAPSHOTDATE)
+  
+  #Monthly mean
+  pchange <- aggregate(myfuture$DIFF,list(myfuture$MONTH),mean)
+  
+  #Find the positive negative counts
+  posnegcount <- myfuture %>% group_by(MONTH) %>%  
+    summarize(pos = sum(DIFF>0), neg = sum(DIFF<0))
+  
+  #Set the date for the future
+  mydate <- format(Sys.time(), "%Y-%m-%d %H:%M:%d")
+  
+  #Insert the values to the database
+  dbSendQuery(mydb,paste("INSERT INTO futuresseasonality VALUES ('",stockticker,"','",mydate,"','MEAN',",noquote(paste(format(round(pchange$x, 2), nsmall = 2),collapse=",")),")",sep=""))
+  dbSendQuery(mydb,paste("INSERT INTO futuresseasonality VALUES ('",stockticker,"','",mydate,"','POS',",noquote(paste(format(round(posnegcount$pos, 2), nsmall = 2),collapse=",")),")",sep=""))
+  dbSendQuery(mydb,paste("INSERT INTO futuresseasonality VALUES ('",stockticker,"','",mydate,"','NEG',",noquote(paste(format(round(posnegcount$neg, 2), nsmall = 2),collapse=",")),")",sep=""))
+  
+  #Close the database
+  dbDisconnect((mydb))
+  
+}
 
-#Monthly differences are at PDifference
-head(mdata)
+
+
+
 
 
