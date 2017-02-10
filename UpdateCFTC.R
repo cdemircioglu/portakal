76d5cf2df2b42da66780f84a05c4c1418185c630
cdemircioglu <cdemircioglu@gmail.com>
2017-02-10 09:00:40 +0000

    new code

--- UpdateCFTC.R
+++ UpdateCFTC.R
@@ -4,7 +4,7 @@ library(Quandl)
 library(RMySQL)
 library(xtable)
 library(data.table)
-#Test git test 2
+
 #Set the server
 myhost <- "cemoptions.cloudapp.net"
 options(stringsAsFactors=F)
@@ -15,9 +15,6 @@ dbSendQuery(mydb, "TRUNCATE TABLE futurescftc;")
 
 #Set the date for the future
 mydate <- format(Sys.time(), "%Y-%m-%d %H:%M:%d")
-
-#Create the resulting data frame
-results = data.frame(FUTURE=NA,SNAPSHOTDATE=NA,REPORTABLELONGS=0.0,NONREPORTABLELONGS=0.0,REPORTABLESHORTS=0.0,NONREPORTABLESHORTS=0.0,OPENINTEREST=0.0,REPOLONGOPENINTEREST=0.0,NONREPOLONGOPENINTEREST=0.0,REPOSHORTOPENINTEREST=0.0,NONREPOSHORTOPENINTEREST=0.0,stringsAsFactors = FALSE)
 
 #Check if the file exists, it not use a constant. 
 if(file.exists("/home/cem/portakal/futures.csv"))
@@ -50,36 +47,17 @@ for(i in 1:nrow(stocktickervector))
   if (stockticker == "ZW")
     stocktickervector[i,3] <- "MW"
   
-  
   #Get the futures
   myfuture <- Quandl(paste("CFTC/",stocktickervector[i,3],"_FO_ALL",sep=""), api_key="zK6coAV1K5eyxuaPvWJm")
   
   #Loop on the rows
-  for (i in 1:nrow(myfuture)){
-  
-    #Get the commercial vs retail 
-    myrow <- c(
-    stockticker,
-    as.character(myfuture$Date[i]),
-    myfuture$`Total Reportable Longs`[i],
-    myfuture$`Non Reportable Longs`[i],
-    myfuture$`Total Reportable Shorts`[i],
-    myfuture$`Non Reportable Shorts`[i],
-    myfuture$`Open Interest`[i],
-    myfuture$`Total Reportable Longs`[i]/myfuture$`Open Interest`[i],
-    myfuture$`Non Reportable Longs`[i]/myfuture$`Open Interest`[i],
-    myfuture$`Total Reportable Shorts`[i]/myfuture$`Open Interest`[i],
-    myfuture$`Non Reportable Shorts`[i]/myfuture$`Open Interest`[i]
-    )
-    
-    #Add them to results
-    #results <- rbind(results,myrow)   
-    
+  for (j in 1:nrow(myfuture)){
+
     #Create the query
     query <- "INSERT INTO futurescftc VALUES ('AAA','BBB',CCC)"
     query <- gsub("AAA", stockticker, query)
-    query <- gsub("BBB", myrow[2], query)
-    query <- gsub("CCC", noquote(paste(myrow[3:length(myrow)],collapse=",")), query) 
+    query <- gsub("BBB", as.character(myfuture[j,1]), query)
+    query <- gsub("CCC", noquote(paste(myfuture[j,c(2:length(myfuture))],collapse=",")), query) 
     
     #Execute the query
     dbSendQuery(mydb, query)   
