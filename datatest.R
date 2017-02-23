28050b7d053b88131b472e579509f942d101de9c
cdemircioglu <cdemircioglu@gmail.com>
2017-02-23 07:47:08 +0000

    Quality check becomes a stored procedure and single column. Soon implement a retry logic on top of it.

--- datatest.R
+++ datatest.R
@@ -4,10 +4,7 @@ library(data.table)
 
 #Connection string
 mydb = dbConnect(MySQL(), user='borsacanavari', password='opsiyoncanavari1', dbname='myoptions', host="cemoptions.cloudapp.net")
-rs = dbSendQuery(mydb, "SELECT A.FUTURE,MAX(CASE WHEN A.VOLUME IS NULL THEN '2015-01-01' ELSE A.SNAPSHOTDATE END) AS CONTINUOUS, MAX(A.SNAPSHOTDATE) AS CURRENT,
-(SELECT MAX(B.SNAPSHOTDATE) FROM futurescftc B WHERE B.FUTURE = A.FUTURE) AS CFTC
-                 FROM futures A GROUP BY A.FUTURE;
-                 ")
+rs = dbSendQuery(mydb, "CALL GetDataQuality();")
 stockdata = fetch(rs, n=-1)
                              
 #Disconnect from the database
